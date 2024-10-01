6e23313920484707a739a83e67351a5754a8d7a7
AshokMadagoni <madagoniashok1@gmail.com>
2024-10-01 22:24:47 +0530

    added

---
+++ task.js
@@ -0,0 +1,84 @@
+// 1.Answer
+
+var a="thursday";
+switch(a){
+    case "monday":
+        console.log("this is monday");
+        break;
+        case "tuesday":
+            console.log("this is tuesday");
+            break;
+            case "wednesday":
+                console.log("this is wednesday");
+                break;
+                case "thursday":
+                    console.log("this is thursday");
+                    break;
+                    case "friday":
+                        console.log("this is friday");
+                        break;
+                        case "saturday":
+                            console.log("this is saturday");
+                            break;
+                            default:
+                                console.log("this is sunday");           
+}
+
+// 2.Answer
+
+var a="august";
+switch(a){
+    case "january":
+        console.log("welcome to January");
+        break;
+        case "february":
+            console.log("welcome to february");
+            break;
+            case "march":
+                console.log("welcome to march");
+                break;
+                case "april":
+                    console.log("welcome to april");
+                    break;
+                    case "may":
+                        console.log("welcome to may");
+                        break;
+                        case "june":
+                            console.log("welcome to june");
+                            break;
+                            case "july":
+                                console.log("welcome to july");
+                                break;
+                                case "august":
+                                    console.log("welcome to August");
+                                    break;
+                                    case "september":
+                                        console.log("welcome to september");
+                                        break;
+                                        case "october":
+                                            console.log("welcome to october");
+                                            break;
+                                            case "novamber":
+                                                console.log("welcome to november");
+                                                break;
+                                                default:
+                                                    console.log("welcome to december");
+}
+
+// 3.Answer
+
+var a=1;
+while(a<=10){
+    if(a%2==0){
+        console.log(a + "is even");
+    }
+    a++
+}
+
+// 4.Answer
+
+var a=10;
+while(a>=0){
+    console.log(a);
+    a--   
+}
